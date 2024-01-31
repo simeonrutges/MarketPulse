0447656fce042507feaa9a69c7913580cf6520b5
simeonrutges <s.rutges@gmail.com>
2024-01-31 11:57:35 +0100

    controller + service category

--- src/main/java/com/example/MarketPulse/service/CategoryService.java
+++ src/main/java/com/example/MarketPulse/service/CategoryService.java
@@ -1,7 +1,58 @@
 package com.example.MarketPulse.service;
 
+import com.example.MarketPulse.dto.CategoryDto;
+import com.example.MarketPulse.exceptions.ResourceNotFoundException;
+import com.example.MarketPulse.model.Category;
+import com.example.MarketPulse.repository.CategoryRepository;
+import com.example.MarketPulse.repository.ProductRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
+    private final CategoryRepository categoryRepository;
+    private final ProductRepository productRepository;
+    private final DtoMapperService dtoMapperService;
+
+    public CategoryService(CategoryRepository categoryRepository, ProductRepository productRepository, DtoMapperService dtoMapperService) {
+        this.categoryRepository = categoryRepository;
+        this.productRepository = productRepository;
+        this.dtoMapperService = dtoMapperService;
+    }
+
+    public CategoryDto createCategory(CategoryDto categoryDto) {
+        Category category = dtoMapperService.categoryDtoToCategory(categoryDto);
+        Category savedCategory = categoryRepository.save(category);
+        return dtoMapperService.categoryToDto(savedCategory);
+    }
+
+    public List<CategoryDto> getAllCategories() {
+        List<Category> categories = categoryRepository.findAll();
+        return categories.stream()
+                .map(dtoMapperService :: categoryToDto)
+                .collect(Collectors.toList());
+    }
+
+    public CategoryDto getCategoryById(Long categoryId) {
+        Category category = categoryRepository.findById(categoryId).
+                orElseThrow(()-> new ResourceNotFoundException("Category not found with ID: " + categoryId));
+        return dtoMapperService.categoryToDto(category);
+    }
+
+    public void deleteCategory(Long categoryId) {
+        if (!categoryRepository.existsById(categoryId)) {
+            throw new ResourceNotFoundException("Category not found with ID: " + categoryId);
+        }
+        categoryRepository.deleteById(categoryId);
+    }
+
+    public CategoryDto updateCategory(Long categoryId, CategoryDto categoryDto) {
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(()-> new ResourceNotFoundException("Category not found with ID: " + categoryId));
+        dtoMapperService.updateCategoryFromDto(category, categoryDto);
+        Category updatedCategory = categoryRepository.save(category);
+        return dtoMapperService.categoryToDto(updatedCategory);
+    }
 }
--- src/main/java/com/example/MarketPulse/service/ProductService.java
+++ src/main/java/com/example/MarketPulse/service/ProductService.java
@@ -1,18 +1,26 @@
 package com.example.MarketPulse.service;
 
 import com.example.MarketPulse.dto.ProductDto;
+import com.example.MarketPulse.exceptions.ResourceNotFoundException;
+import com.example.MarketPulse.model.Category;
 import com.example.MarketPulse.model.Product;
+import com.example.MarketPulse.repository.CategoryRepository;
 import com.example.MarketPulse.repository.ProductRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
     private final DtoMapperService dtoMapperService;
+    private final CategoryRepository categoryRepository;
 
-    public ProductService(ProductRepository productRepository, DtoMapperService dtoMapperService) {
+    public ProductService(ProductRepository productRepository, DtoMapperService dtoMapperService, CategoryRepository categoryRepository) {
         this.productRepository = productRepository;
         this.dtoMapperService = dtoMapperService;
+        this.categoryRepository = categoryRepository;
     }
 
     public ProductDto createProduct(ProductDto productDto) {
@@ -21,5 +29,18 @@ public class ProductService {
         Product savedProduct = productRepository.save(product);
 
         return dtoMapperService.productToProductDto(savedProduct);
+    }
+
+    public List<ProductDto> getProductsByCategory(Long categoryId) {
+
+        boolean categoryExists = categoryRepository.existsById(categoryId);
+        if (!categoryExists) {
+            throw new ResourceNotFoundException("Category not found with ID: " + categoryId);
+        }
+        List <Product> products = productRepository.findByCategoryId(categoryId);
+
+        return products.stream()
+            .map(dtoMapperService :: productToProductDto)
+            .collect(Collectors.toList());
     }
 }
