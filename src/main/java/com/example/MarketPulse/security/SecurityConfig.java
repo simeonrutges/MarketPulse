279fbbe391763c77ab114d1d23f60f43b3d7128d
simeonrutges <s.rutges@gmail.com>
2024-02-07 09:33:29 +0100

    basis toegang gemaakt

--- src/main/java/com/example/MarketPulse/security/SecurityConfig.java
+++ src/main/java/com/example/MarketPulse/security/SecurityConfig.java
@@ -175,25 +175,49 @@ public class SecurityConfig {
 //                        .anyRequest().authenticated()
 
                         // Specifieke pad- en methodecombinaties
-                        .requestMatchers(HttpMethod.POST, "/users").permitAll() // Iedereen kan registreren
-                        .requestMatchers(HttpMethod.POST, "/auth").permitAll() // Iedereen kan inloggen
+                        .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/roles").permitAll()
 
-                        // Specifieke padpatronen voor bepaalde rollen of autoriteiten
-                        .requestMatchers(HttpMethod.DELETE, "/users").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.GET, "/admin/stats/**").hasRole("ADMIN") // Alternatieve manier om rollen te specificeren
+                        // toegang tot users
+                        .requestMatchers(HttpMethod.DELETE, "/users/**").hasAuthority("ADMIN")
                                  .requestMatchers(HttpMethod.GET, "/users").hasAuthority("ADMIN")
+                                // toegangscontrole products
+                                .requestMatchers(HttpMethod.POST, "/products").hasAnyAuthority("ADMIN", "SELLER")
+                                .requestMatchers(HttpMethod.PUT, "/products/**").hasAnyAuthority("ADMIN", "SELLER")
+                                .requestMatchers(HttpMethod.DELETE, "/products/**").hasAnyAuthority("ADMIN", "SELLER")
+                                // Toegang tot categories
+                                .requestMatchers(HttpMethod.POST, "/categories/**").hasAuthority("ADMIN")
+                                .requestMatchers(HttpMethod.PUT, "/categories/**").hasAuthority("ADMIN")
+                                .requestMatchers(HttpMethod.DELETE, "/categories/**").hasAuthority("ADMIN")
+                                // Toegang tot orders
+                                .requestMatchers(HttpMethod.GET, "/orders/**").hasAnyAuthority("ADMIN", "SELLER", "BUYER") // ADMIN, SELLER, en BUYER kunnen alle orders bekijken
+                                .requestMatchers(HttpMethod.POST, "/orders/create/**").hasAnyAuthority("SELLER", "BUYER") // SELLER en BUYER kunnen orders creëren
+                                .requestMatchers(HttpMethod.PUT, "/orders/**").hasAnyAuthority("ADMIN", "SELLER", "BUYER") // ADMIN, SELLER, en BUYER kunnen orders bijwerken
+                                .requestMatchers(HttpMethod.DELETE, "/orders/**").hasAuthority("ADMIN") // Alleen ADMIN kan orders verwijderen
+                                .requestMatchers(HttpMethod.GET, "/orders/users/**").hasAnyAuthority("ADMIN", "SELLER", "BUYER") // ADMIN, SELLER, en BUYER kunnen orders per gebruiker bekijken
+                                // Toegang tot transacties
+                                .requestMatchers(HttpMethod.POST, "/transactions").hasAnyAuthority("SELLER", "BUYER") // SELLER en BUYER kunnen transacties creëren
+                                .requestMatchers(HttpMethod.GET, "/transactions").hasAnyAuthority("ADMIN", "SELLER", "BUYER") // ADMIN, SELLER, en BUYER kunnen alle transacties bekijken
+                                .requestMatchers(HttpMethod.GET, "/transactions/users/**").hasAnyAuthority("ADMIN", "SELLER", "BUYER") // ADMIN, SELLER, en BUYER kunnen transacties per gebruiker bekijken
+                                .requestMatchers(HttpMethod.GET, "/transactions/**").hasAnyAuthority("ADMIN", "SELLER", "BUYER") // ADMIN, SELLER, en BUYER kunnen specifieke transacties bekijken
+                                .requestMatchers(HttpMethod.PATCH, "/transactions/**").hasAnyAuthority("ADMIN", "SELLER", "BUYER") // ADMIN, SELLER, en BUYER kunnen transacties bijwerken
+                                .requestMatchers(HttpMethod.DELETE, "/transactions/**").hasAuthority("ADMIN") // Alleen ADMIN kan transacties verwijderen
+                                // Toegangsregels voor winkelwagens
+                                .requestMatchers(HttpMethod.GET, "/carts/**").hasAnyAuthority("ADMIN", "SELLER", "BUYER")
+                                // Toegangsregels voor winkelwagenitems
+                                .requestMatchers(HttpMethod.POST, "/cart-items/users/**").hasAnyAuthority("SELLER", "BUYER") // Toestaan dat SELLER en BUYER items toevoegen aan de winkelwagen
+                                .requestMatchers(HttpMethod.DELETE, "/cart-items/**").hasAnyAuthority("SELLER", "BUYER") // Toestaan dat SELLER en BUYER items verwijderen uit hun winkelwagen
+                                .requestMatchers(HttpMethod.PUT, "/cart-items/**").hasAnyAuthority("SELLER", "BUYER") // Toestaan dat SELLER en BUYER items in hun winkelwagen bijwerken
+                                // Toegang tot reviews
+                                .requestMatchers(HttpMethod.GET, "/reviews/**").permitAll() // Iedereen kan alle types van review verzoeken bekijken
+                                .requestMatchers(HttpMethod.POST, "/reviews").hasAnyAuthority("BUYER") // Alleen BUYERs kunnen reviews creëren
+                                .requestMatchers(HttpMethod.PUT, "/reviews/{reviewId}").hasAnyAuthority("ADMIN", "BUYER") // ADMIN en de originele BUYER kunnen reviews bijwerken
+                                .requestMatchers(HttpMethod.DELETE, "/reviews/{reviewId}").hasAuthority("ADMIN") // Alleen ADMIN kan reviews verwijderen
 
-                        // Rollengebaseerde toegangscontrole voor bepaalde paden
-                        .requestMatchers("/secured/**").authenticated() // Vereist authenticatie voor toegang tot beveiligde paden
-                        .requestMatchers("/admin/**").hasAuthority("ADMIN") // Algemeen admin pad alleen voor ADMIN
-
-                        // Algemene regels
-                        .requestMatchers("/public/**").permitAll() // Publieke paden voor iedereen toegankelijk
+                                // Algemene regels
                                 .requestMatchers("/users/**").permitAll()
-                                .requestMatchers("/auth/**)").permitAll()
-
                         .anyRequest().authenticated() // Alle andere verzoeken vereisen authenticatie
-
                 )
                 // JWT Token filter voor authenticatie
                 .addFilterBefore(new JwtRequestFilter(jwtService, userDetailsService()), UsernamePasswordAuthenticationFilter.class);
